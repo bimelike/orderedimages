cf9b3891ad6de87f5a9bbd4cdaab8518b15da6ac
bimelike <bimelike@hotmail.com>
2022-11-15 19:52:12 +0300

    Ordered Images

---
+++ main.js
@@ -0,0 +1,49 @@
+const wait = function (seconds) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, seconds * 1000)
+    })
+}
+
+let image_container = document.getElementById('images-container');
+let row = document.createElement('div');
+row.classList.add('row', 'w-50');
+
+const createImage = function (imgPath) {
+
+    return new Promise((resolve, reject) => {
+        let img = document.createElement('img');
+        img.addEventListener('load', function (e) {
+            resolve(img);
+        });
+        img.addEventListener('error', function (e) {
+            reject(e.message);
+        });
+
+        img.src = imgPath;
+    });
+}
+
+async function loadImages() {
+
+    let img = await createImage(`/img/img-1.jpg`)
+
+    addImage(img);
+    await wait(2);
+    img.classList.add('hidden');
+
+    img = await createImage(`/img/img-2.jpg`);
+    addImage(img);
+    await wait(2);
+    img.classList.add('hidden');
+
+    img = await createImage(`/img/img-3.jpg`);
+    addImage(img);
+
+}
+
+function addImage(img) {
+    row.appendChild(img);
+    image_container.appendChild(row);
+}
+
+loadImages();
