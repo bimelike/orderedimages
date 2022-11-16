d2c0e8011e124b67f0e1b032d080818c7e1efec3
bimelike <bimelike@hotmail.com>
2022-11-16 18:25:22 +0300

    challenge 2

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ let image_container = document.getElementById('images-container');
 let row = document.createElement('div');
 row.classList.add('row', 'w-50');
 
+let btnLoadNPause = document.getElementById('btnLoadNPause');
+let btnLoadAll = document.getElementById('btnLoadAll');
+
 const createImage = function (imgPath) {
 
     return new Promise((resolve, reject) => {
@@ -23,18 +26,18 @@ const createImage = function (imgPath) {
     });
 }
 
-async function loadImages() {
+async function loadNPause() {
 
     let img = await createImage(`./img/img-1.jpg`)
 
     addImage(img);
     await wait(2);
-    img.classList.add('hidden');
+    img.style.display = 'none';
 
     img = await createImage(`./img/img-2.jpg`);
     addImage(img);
     await wait(2);
-    img.classList.add('hidden');
+    img.style.display = 'none';
 
     img = await createImage(`./img/img-3.jpg`);
     addImage(img);
@@ -46,4 +49,33 @@ function addImage(img) {
     image_container.appendChild(row);
 }
 
-loadImages();
+async function loadAll(imgArr) {
+    let imgs = imgArr.map(imgPath => createImage(imgPath))
+
+    console.log(imgs);
+
+    Promise.all(imgs).then((elements) => {
+        elements.forEach(item => {
+            addImage(item);
+        })
+    })
+}
+
+btnLoadAll.addEventListener('click', function () {
+    loadAll([
+        './img/img-1.jpg',
+        './img/img-2.jpg',
+        './img/img-3.jpg'
+    ]);
+});
+
+btnLoadNPause.addEventListener('click', function () {
+    loadNPause();
+});
+
+//loadNPause();
+// loadAll([
+//     './img/img-1.jpg',
+//     './img/img-2.jpg',
+//     './img/img-3.jpg'
+// ]);
